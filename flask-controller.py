ba73123dd55443a4b03b4de1d6ab98c4b97f2f99
Maxim <osadchuk.m.01@gmail.com>
2017-07-13 20:36:04 +0300

    Added create-app function.

--- flask-controller.py
+++ flask-controller.py
@@ -1,6 +1,7 @@
 import os
 import scriptine
 
+
 PROJECT_TREE = {
     'project_name': {
         'app': {
@@ -22,6 +23,14 @@ PROJECT_TREE = {
     }
 }
 
+APP_TREE = {
+    'app_name': {
+        'files': ['__init__.py', 'views.py', 'forms.py', 'errors.py']
+    }
+}
+
+
+
 def make_files_from_list(files, current_dir='./'):
 	for f in files:
 		open(os.path.join(current_dir, f), 'a').close()
@@ -41,17 +50,27 @@ def make_dirs_from_dict(d, current_dir='./'):
             if type(val) == dict:
                 make_dirs_from_dict(val, os.path.join(current_dir, key))
 
-def create_project_command(name):
+def create_project_command(project_name):
     """
     Create flask project tree.
     
-    :param name: project name
+    :param project_name: project name
     """
     project_tree = {}
-    project_tree[name] = PROJECT_TREE.pop('project_name')
+    project_tree[project_name] = PROJECT_TREE.pop('project_name')
     make_dirs_from_dict(project_tree)
     
+def create_app_command(app_name, path='./'):
+    """
+    Create flask app tree.
+
+    :param app_name: application name
+    :param path: path/to/your/app
+    """
+    app_tree = {}
+    app_tree[app_name] = APP_TREE.pop('app_name')
+    make_dirs_from_dict(app_tree, path)
+
 
 if __name__ == '__main__':
     scriptine.run()
-
